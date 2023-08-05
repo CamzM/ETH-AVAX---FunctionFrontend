2a990b001ac778d3e7ddee5ebf2e7bf972d55242
CamzM <96362170+CamzM@users.noreply.github.com>
2023-08-06 00:22:34 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -25,20 +25,15 @@ contract Assessment {
     function deposit(uint256 _amount) public payable {
         uint256 _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -48,44 +43,35 @@ contract Assessment {
             revert InsufficientBalance({balance: balance, withdrawAmount: _withdrawAmount});
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
     }
 
     function divideBalance(uint256 _multiplier) public {
-        // make sure this is the owner
+
         require(msg.sender == owner, "You are not the owner of this account");
 
         uint256 _previousBalance = balance;
 
-        // multiply the balance
         balance /= _multiplier;
 
-        // assert the balance is correct
         assert(balance == _previousBalance / _multiplier);
 
-        // emit the event
         emit BalanceDivided(_previousBalance, balance);
     }
     function multiplyBalance(uint256 _multiplier) public {
-        // make sure this is the owner
+
         require(msg.sender == owner, "You are not the owner of this account");
 
         uint256 _previousBalance = balance;
 
-        // multiply the balance
         balance *= _multiplier;
 
-        // assert the balance is correct
         assert(balance == _previousBalance * _multiplier);
 
-        // emit the event
         emit BalanceMultiplied(_previousBalance, balance);
     }
     
